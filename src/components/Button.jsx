b13828435174b05db9a9948de0b2920440328be2
Dev Chauhan <devccode8@gmail.com>
2024-01-16 22:45:41 +0530

    update register link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,7 @@ const Button = ({ styles }) => (
     data-aos="zoom-in"
     data-aos-duration="500"
     data-aos-easing="ease-in-out"
-<<<<<<< HEAD
     href="https://www.commudle.com/communities/fetch-ai-developers-meerut/events/f-tech-hackathon"
-=======
-    href="https://www.commudle.com/fill-form/2141"
->>>>>>> 3f2bf15 (picture add)
     target="_blank"
     type="button"
     className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles} shadow-md shadow-sky-500 `}
